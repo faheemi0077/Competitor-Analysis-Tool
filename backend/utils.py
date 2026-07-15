9c80c48e8c649d979fa168d048cbbc1c400cd7fc
faheemi0077 <iqbalfaheem0077@gmail.com>
2026-07-15 10:05:29 -0700

    -m

--- backend/utils.py
+++ backend/utils.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 client = anthropic.Anthropic()
+#apify_client alias for standard measure
 the_apify_client = apify_client = ApifyClient(os.environ["APIFY_TOKEN"])
 
 
